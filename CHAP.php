3b559a52003c48f4ebcc84588fce0c086280630f
Fabrizio Catalucci <f.catalucci@unidata.it>
2019-01-31 17:12:05 +0100

    Removed requires (and some other warning)

--- CHAP.php
+++ CHAP.php
@@ -32,8 +32,6 @@ any other GPL-like (LGPL, GPL2) License.
     $Id$
 */
 
-require_once 'PEAR.php';
-
 /**
 * Classes for generating packets for various CHAP Protocols:
 * CHAP-MD5: RFC1994
@@ -65,7 +63,7 @@ class Crypt_CHAP extends PEAR
      * Binary response
      * @var  string
      */
-    var $response = null;    
+    var $response = null;
 
     /**
      * User password
@@ -85,9 +83,9 @@ class Crypt_CHAP extends PEAR
      * Generates a random challenge
      * @return void
      */
-    function Crypt_CHAP()
+    function __construct()
     {
-        $this->PEAR();
+        parent::__construct();
         $this->generateChallenge();
     }
     
@@ -96,7 +94,7 @@ class Crypt_CHAP extends PEAR
      *
      * @param  string  $varname  Name of the property
      * @param  integer $size     Size of the challenge in Bytes
-     * @return void
+     * @return string
      */
     function generateChallenge($varname = 'challenge', $size = 8)
     {
@@ -167,9 +165,9 @@ class Crypt_CHAP_MSv1 extends Crypt_CHAP
      * Loads the hash extension
      * @return void
      */
-    function Crypt_CHAP_MSv1()
+    function __construct()
     {
-        $this->Crypt_CHAP();
+        parent::__construct();
         $this->loadExtension('hash');        
     }
     
@@ -415,9 +413,9 @@ class Crypt_CHAP_MSv2 extends Crypt_CHAP_MSv1
      * Generates the 16 Bytes peer and authentication challenge
      * @return void
      */
-    function Crypt_CHAP_MSv2()
+    function __construct()
     {
-        $this->Crypt_CHAP_MSv1();
+        parent::__construct();
         $this->generateChallenge('peerChallenge', 16);
         $this->generateChallenge('authChallenge', 16);
     }    
@@ -458,6 +456,3 @@ class Crypt_CHAP_MSv2 extends Crypt_CHAP_MSv1
         return $this->_challengeResponse();
     }    
 }
-
-
-?>
